a41685e53a8b30f86c6946b5da921094e4f173b9
Markus Mayer <code@defx.de>
2008-06-03 00:49:28 +0200

    Label mit grauem Hintergrund, Beenden-Warnung wieder entfernt

--- gui.py
+++ gui.py
@@ -4,20 +4,30 @@
 import sys
 from PyQt4 import QtGui, QtCore
 
-class MyForm(QtGui.QMainWindow):
+class QDisplay(QtGui.QWidget):
 	def __init__(self, parent=None):
 		QtGui.QWidget.__init__(self, parent)
+		
+		self.setAutoFillBackground(True)
+		
+		palette = self.palette()
+		palette.setColor(QtGui.QPalette.Background, QtGui.QColor(32, 32, 32))
+		self.setPalette(palette)
+
+class MyForm(QtGui.QMainWindow):
+	def __init__(self, parent=None):
+		QtGui.QMainWindow.__init__(self, parent)
 
 		# Fensterstatus setzen
-		self.resize(250, 150)
+		self.resize(640, 480)
 		self.setWindowTitle("Testanwendung")
 		self.setWindowIcon(QtGui.QIcon("icons/icon.gif"))
 		self.center()
 		self.statusBar().showMessage("Programm gestartet.")
 		
-		textEdit = QtGui.QTextEdit()
-		textEdit.setText("Dies ist das zentrale Widget.<br />Yeah.")
-		self.setCentralWidget(textEdit)
+		# Das Display-Widget
+		displayArea = QDisplay()
+		self.setCentralWidget(displayArea)
 
 		
 		# Menüeintrag zum Beenden
@@ -34,13 +44,13 @@ class MyForm(QtGui.QMainWindow):
 
 
 	def closeEvent(self, event):
-		reply = QtGui.QMessageBox.question(self, "Programm beenden",
-			"Sind Sie sicher?", QtGui.QMessageBox.Yes, QtGui.QMessageBox.No)
+		#reply = QtGui.QMessageBox.question(self, "Programm beenden", "Sind Sie sicher?", QtGui.QMessageBox.Yes, QtGui.QMessageBox.No)
 
-		if reply == QtGui.QMessageBox.Yes:
-			event.accept()
-		else:
-			event.ignore()
+		#if reply == QtGui.QMessageBox.Yes:
+		#	event.accept()
+		#else:
+		#	event.ignore()
+		return
 
 	def center(self):
 		screen = QtGui.QDesktopWidget().screenGeometry()
@@ -48,15 +58,17 @@ class MyForm(QtGui.QMainWindow):
 		self.move((screen.width()-size.width())/2, (screen.height()-size.height())/2)
 
 
-app = QtGui.QApplication(sys.argv)
+# Die Anwendung nur starten, wenn die Source nicht als Modul geladen wird
+if __name__ == "__main__":
+	app = QtGui.QApplication(sys.argv)
 
-# Siehe: http://www.riverbankcomputing.co.uk/static/Docs/PyQt4/html/qtranslator.html
-translator = QtCore.QTranslator()
-translator.load("qt_de", "/usr/share/qt4/translations")
-app.installTranslator(translator)
+	# Siehe: http://www.riverbankcomputing.co.uk/static/Docs/PyQt4/html/qtranslator.html
+	translator = QtCore.QTranslator()
+	translator.load("qt_de", "/usr/share/qt4/translations")
+	app.installTranslator(translator)
 
-form = MyForm()
-form.show()
+	form = MyForm()
+	form.show()
 
-sys.exit(app.exec_())
+	sys.exit(app.exec_())
 
