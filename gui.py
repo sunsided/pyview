5a7abf1c88485666a43c0999267a85e1a1e73a18
Markus Mayer <code@defx.de>
2008-06-03 00:52:17 +0200

    Etwas aufgeräumter dank "from PyQt4.QtGui import *"

--- gui.py
+++ gui.py
@@ -2,26 +2,27 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt4 import QtGui, QtCore
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
 
-class QDisplay(QtGui.QWidget):
+class QDisplay(QWidget):
 	def __init__(self, parent=None):
-		QtGui.QWidget.__init__(self, parent)
+		QWidget.__init__(self, parent)
 		
 		self.setAutoFillBackground(True)
 		
 		palette = self.palette()
-		palette.setColor(QtGui.QPalette.Background, QtGui.QColor(32, 32, 32))
+		palette.setColor(QPalette.Background, QColor(32, 32, 32))
 		self.setPalette(palette)
 
-class MyForm(QtGui.QMainWindow):
+class MyForm(QMainWindow):
 	def __init__(self, parent=None):
-		QtGui.QMainWindow.__init__(self, parent)
+		QMainWindow.__init__(self, parent)
 
 		# Fensterstatus setzen
 		self.resize(640, 480)
 		self.setWindowTitle("Testanwendung")
-		self.setWindowIcon(QtGui.QIcon("icons/icon.gif"))
+		self.setWindowIcon(QIcon("icons/icon.gif"))
 		self.center()
 		self.statusBar().showMessage("Programm gestartet.")
 		
@@ -31,10 +32,10 @@ class MyForm(QtGui.QMainWindow):
 
 		
 		# Menüeintrag zum Beenden
-		exit = QtGui.QAction(QtGui.QIcon("icons/exit.gif"), "&Beenden", self)
+		exit = QAction(QIcon("icons/exit.gif"), "&Beenden", self)
 		exit.setShortcut("ESC")
 		exit.setStatusTip("Beendet das Programm")
-		self.connect(exit, QtCore.SIGNAL("triggered()"), QtCore.SLOT("close()"))
+		self.connect(exit, SIGNAL("triggered()"), SLOT("close()"))
 		
 		# Menüzeile
 		# http://zetcode.com/tutorials/pyqt4/menusandtoolbars/
@@ -44,26 +45,26 @@ class MyForm(QtGui.QMainWindow):
 
 
 	def closeEvent(self, event):
-		#reply = QtGui.QMessageBox.question(self, "Programm beenden", "Sind Sie sicher?", QtGui.QMessageBox.Yes, QtGui.QMessageBox.No)
+		#reply = QMessageBox.question(self, "Programm beenden", "Sind Sie sicher?", QMessageBox.Yes, QMessageBox.No)
 
-		#if reply == QtGui.QMessageBox.Yes:
+		#if reply == QMessageBox.Yes:
 		#	event.accept()
 		#else:
 		#	event.ignore()
 		return
 
 	def center(self):
-		screen = QtGui.QDesktopWidget().screenGeometry()
+		screen = QDesktopWidget().screenGeometry()
 		size =  self.geometry()
 		self.move((screen.width()-size.width())/2, (screen.height()-size.height())/2)
 
 
 # Die Anwendung nur starten, wenn die Source nicht als Modul geladen wird
 if __name__ == "__main__":
-	app = QtGui.QApplication(sys.argv)
+	app = QApplication(sys.argv)
 
 	# Siehe: http://www.riverbankcomputing.co.uk/static/Docs/PyQt4/html/qtranslator.html
-	translator = QtCore.QTranslator()
+	translator = QTranslator()
 	translator.load("qt_de", "/usr/share/qt4/translations")
 	app.installTranslator(translator)
 
